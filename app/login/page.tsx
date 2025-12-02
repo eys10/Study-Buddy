f2378346c3f03c1597ef2796d13a46dc1643ba7e
eys10 <johnacepadilla08@gmail.com>
2025-12-02 11:26:32 +0800

    added login route

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Login(){
+    return(
+        <section>
+            <div>LOGIN PAGE</div>
+            <div>
+                <Link href="/">Back to Home</Link>
+            </div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="">
-        <h1>HELLO GUYsdfdsfsdS</h1>
+        <h1>HOME</h1>
+        <Link href="/login">to Login</Link>
     </div>
   );
 }
